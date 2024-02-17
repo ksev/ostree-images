78beb17aa48e6c875be5e2188e3dcec7d2c51f3d
Kim Sevandersson <kim@kotee.co>
2024-02-17 19:43:56 +0100

    Auto mount bcachefs

---
+++ images/k3s-node/ansible/files/bcachefs-mount.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mount -t bcachefs /dev/sda:/dev/sdb:/dev/sdc /mnt/storage/
+mount --bind /mnt/storage/Vault /var/share/Vault
+mount --bind /mnt/storage/VaultRO /var/share/VaultRO
+mount --bind /mnt/storage/Media /var/share/Media
+mount --bind /mnt/storage/Downloads /var/share/Downloads
