c2d7399c83f830e8c5933778da2d9ed83c60b749
Kim Sevandersson <kim@kotee.co>
2024-02-18 10:48:31 +0100

    update things

--- images/k3s-node/ansible/files/bcachefs-mount.sh
+++ images/k3s-node/ansible/files/bcachefs-mount.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mount -t bcachefs /dev/sda:/dev/sdb:/dev/sdc /mnt/storage/
 mount --bind /mnt/storage/Vault /var/share/Vault
 mount --bind /mnt/storage/VaultRO /var/share/VaultRO
